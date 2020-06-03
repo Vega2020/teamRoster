8cdd57c0ef7cdc318cd43d870729fa95d0a6a2bf
Jeremy Flowers <flowersje08@gmail.com>
2020-06-02 22:28:03 -0700

    inquirer still working but not able to make it repeat

--- app.js
+++ app.js
@@ -18,12 +18,20 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+//empty array to push our employees to as we buid our roster
+const employeeRoster = [];
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 //inquirer prompt to ask about each team member, with info from their class
 
 //STEP: create an array for the employees to go in?
 //STEP: write a for each that does the below to all the objects?
+
+
+function enterData() {
+
 inquirer
     .prompt([
         {
@@ -40,6 +48,7 @@ inquirer
             when: function(answers) {
               return answers.role === "Engineer";
             }
+            //try making the above a double == so you don't have to worry about capitalization
         },
         {
             name: "officeNumber",
@@ -64,28 +73,23 @@ inquirer
             message: "Enter the employee's ID number:"
         },
     ]).then(function(user){
-            console.log(user);
-        //we'll use if-else statements here to render the different function depending on the entered employee's role
+        employeeRoster.push(user);
+        console.log(employeeRoster);
         
-    });
+    });//closing bracket of .then code
 
+};//closing bracket of enterdata function
+    
+enterData();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// Hint: you may need to check if the `output` folder exists and create it if it does not.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//FINISHED:
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different information; write your code to ask different questions via inquirer depending on employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer, and Intern classes should all extend from a class named Employee; see the directions for further information. Be sure to test out each class and verify it generates an object with the correct structure and methods. This structure will be crucial in order for the provided `render` function to work! ```
